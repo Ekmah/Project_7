ee233c174330684fdf3f80e40f9d3b1c8eeba669
Thomas Poupinet <th.poupinet@free.fr>
2021-09-29 07:50:43 +0200

    spread js to correct files

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,56 +1,21 @@
 //---------------------------------------------------------------------------------------------------//
-//-------------------------------------SIGNUP WORKING------------------------------------------------//
-//---------------------------------------------------------------------------------------------------//
-// payload = {
-//     "username": "toto",
-//     "email_adress": "toto.toto@toto.fr",
-//     "password": "147896325"
-// }
-// console.log(payload)
-// fetch(`http://localhost:4000/api/auth/signup`, {
-// headers: {
-//     'Content-Type': "application/json"
-// },
-// method: 'POST',
-// body: JSON.stringify(payload)
-// })
-//---------------------------------------------------------------------------------------------------//
-//--------------------------------------LOGIN WORKING------------------------------------------------//
-//---------------------------------------------------------------------------------------------------//
-payload = {
-    "email_adress": "toto.toto@toto.fr",
-    "password": "147896325"
-}
-fetch(`http://localhost:4000/api/auth/login`, {
-headers: {
-    'Content-Type': "application/json"
-},
-method: 'POST',
-body: JSON.stringify(payload)
-})
-//---------------------------------------------------------------------------------------------------//
 //---------------------------------GET ALL THREADS WORKING-------------------------------------------//
 //---------------------------------------------------------------------------------------------------//
-.then(response => {
-    console.log(response)
-    return response.json()  
+
+console.log(res)
+const token=res.token
+fetch(`http://localhost:4000/api/threads/`, {
+headers: {
+    'Authorization': token
+}
 })
-.then(function(res){
-    console.log(res)
-    const token=res.token
-    fetch(`http://localhost:4000/api/threads/`, {
-    headers: {
-        'Authorization': token
+.then(function(res) {
+    if (res.ok) {
+        return res.json();
     }
-    })
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function(things) {
-        console.log(things)
-    })
+})
+.then(function(things) {
+    console.log(things)
 })
 
 

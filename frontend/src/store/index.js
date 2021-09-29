736a98279af250ae211b038f274d20fbb6d2e74a
Thomas Poupinet <th.poupinet@free.fr>
2021-09-29 09:05:17 +0200

    setup vue cli

---
+++ frontend/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    token:0
+  },
+  mutations: {
+    INSERT_TOKEN(state, payload) {
+      state.token = payload
+    }
+  },
+  actions: {
+    insert_token(context, token) {
+      context.commit('INSERT_TOKEN', token)
+    }
+  },
+  modules: {
+  }
+})

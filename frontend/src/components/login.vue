91e373ac00abcc095fa4563929c26518a6287ffb
Thomas Poupinet <th.poupinet@free.fr>
2021-10-06 08:11:11 +0200

    router add again bcuze problem

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,24 +1,20 @@
 <script>
 export default {
-    name: "LogIn",
-}
-payload = {
-    "email_adress": "toto.toto@toto.fr",
-    "password": "147896325"
-}
-fetch(`http://localhost:4000/api/auth/login`, {
-headers: {
-    'Content-Type': "application/json"
-},
-method: 'POST',
-body: JSON.stringify(payload)
-})
+  name: "LogIn",
+};
+// payload = {
+//   email_adress: "toto.toto@toto.fr",
+//   password: "147896325",
+// };
+// fetch(`http://localhost:4000/api/auth/login`, {
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   method: "POST",
+//   body: JSON.stringify(payload),
+// });
 </script>
 
-<template>
-    
-</template>
+<template></template>
 
-<style>
-
-</style>
+<style></style>

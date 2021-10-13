d6a01efa76541104d1f8c00be8e0606b17a7f578
Thomas Poupinet <th.poupinet@free.fr>
2021-10-13 09:25:59 +0200

    update

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token:null
   },
   mutations: {
+    INSERT_TOKEN(state, payload) {
+      state.token = payload
+    }
   },
   actions: {
+    insert_token(context, token) {
+      context.commit('INSERT_TOKEN', token)
+    }
   },
   modules: {
   }
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <LogIn></LogIn>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import LogIn from '@/components/LogIn.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    LogIn
+  }
+}
+</script>
